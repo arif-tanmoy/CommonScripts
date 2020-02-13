63526efa47d826858227449c89629045ee76b729
arif tanmoy <26651539+arif-tanmoy@users.noreply.github.com>
2020-02-13 11:25:05 +0600

    Add files via upload

---
+++ FastaSeq_extract.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+'''
+Author: Arif Mohammad Tanmoy (arif.tanmoy@gmail.com)
+Use: python(2.7) <Genbank file with Sequence> <VCF file> <Output_file> <Locus_tags separated by (,)comma> <Phred-score (optional)> <Read_proportion (optional)>
+python packages: Biopython (tested with version 1.72), argparse.
+'''
+from Bio import SeqIO
+from argparse import (ArgumentParser, FileType)
+
+def parse_args():
+	"Parse the input arguments, use '-h' for help"
+	commands = ArgumentParser(description='Extract query sequence from FASTA file.')
+	commands.add_argument('--fasta', type=str, required=True, help='Fasta Sequence file (Required).')
+	commands.add_argument('--query', type=str, required=True, help='query ID file (Required)')
+	commands.add_argument('--out', type=str, required=False, help='Output file with query sequence.', default='Output.fasta')
+	return commands.parse_args()
+args = parse_args()
+
+# Let's define a finding tool
+def find_id(idlist, qid):
+	for i in range(0, len(idlist)):
+		if qid in idlist[i]:
+			return i
+	return 'Not Found'
+
+idd = []
+seq = []
+for rec in SeqIO.parse(open(args.fasta,"r"), 'fasta'):
+	idd.append(str(rec.id))
+	seq.append(str(rec.seq))
+
+query = []
+for line in open(args.query,"r"):
+	query.append(line.rstrip())
+
+output = open(args.out,"w")
+for i in range(0, len(query)):
+	j = find_id(idd, str(query[i]))
+	if j != 'Not Found':
+		print 'Found: '+str(query[i])
+		result = ">" + str(idd[j]) + '\n' + str(seq[j])+ '\n'
+		output.write(result)
+
+
+'''
+for i in range(0, len(query)):
+	j = idd.find(str(query[i]))
+	if j == -1:
+		pass
+	else:
+		print str(query[i])
+		k = idd.index(str(query[i]))
+		result = ">" + str(idd[k]) + '\n' + str(seq[k])+ '\n'
+		output.write(result)
+'''
